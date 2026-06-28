cbf8b84f57a4a20f5542b1c79c660a6742ad1609
kamthornsa <kamthorn.sa@ksu.ac.th>
2026-06-28 20:05:43 +0700

    basepath

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  // basePath: "/pb",
+  basePath: isProd ? "/prompt-banking" : "",
   images: {
     remotePatterns: [
       {
